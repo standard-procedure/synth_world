ede9827fab44df04450a575af3781f4b71ed2feb
Rahoul Baruah <rahoulb@echodek.co>
2026-05-07 12:35:58 +0100

    Empty Gem

---
+++ lib/synth_world.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "synth_world/version"
+
+module SynthWorld
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/synth_world/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SynthWorld
+  VERSION = "0.1.0"
+end
